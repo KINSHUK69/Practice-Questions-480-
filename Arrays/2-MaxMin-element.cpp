f6ebb52b7893cf145511b020f6baf12cb69ee11f
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-01-18 00:48:01 +0530

    Create 2-MaxMin-element.cpp

---
+++ Arrays/2-MaxMin-element.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int max(int arr[],int n)
+{
+  sort(arr,arr+n-1);
+  return arr[n-1];
+}
+
+int min(int arr[],int n)
+{
+  sort(arr,arr+n-1);
+  return arr[0];
+}
