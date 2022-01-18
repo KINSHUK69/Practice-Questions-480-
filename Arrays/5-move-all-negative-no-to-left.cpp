7ef7d8110a2a9e953a4e3773be7b333d316d2c9f
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-01-18 16:16:43 +0530

    Create 5-move-all-negative-no-to-left.cpp

---
+++ Arrays/5-move-all-negative-no-to-left.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+// Method : 0
+ void solve(int arr[]){
+   int n=
+ }
+
+// Method 1
+void move(int arr[],int s,int l)       // Time Complexity : O(n)   where n is the size of the array     Space Complexity : O(1) 
+{
+  int j=0;
+  int n=sizeof()
+  for(int i=0;i<n;i++)
+  {
+    if(arr[i]<0)
+    {
+      if(i!=j)
+        swap(arr[i],arr[j]);
+      j++;      
+    }
+  }
+}
+
+// Method : 2
+void TwoPointer(int a[],int l,int r)
+{
+  
+}
