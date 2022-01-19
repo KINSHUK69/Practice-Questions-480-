8f80eb01869086746c337d53178b713ab39b13ae
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-01-19 21:55:20 +0530

    Create 7-rotate-array.cpp

---
+++ Arrays/7-rotate-array.cpp
@@ -0,0 +1,21 @@
+
+
+void rotate(int arr[], int n)   // using two pointers
+{
+   int left=0,right=n-1;
+    while(left<right)
+    {
+        swap(arr[left],arr[right]);
+        left++;
+    }
+}
+
+void rotate(int arr[],int n)    // shifting the array elements to the right 
+{
+  int f=a[n-1];
+   for(int i=n-2;i>=0;i--)
+    {
+        a[i+1]=arr[i];
+    }
+   a[0]=f;
+}
