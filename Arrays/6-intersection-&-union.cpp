835669c54e497f5370b3dc169f167e144af9677c
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-01-19 21:13:17 +0530

    Update 6-intersection-&-union.cpp

--- Arrays/6-intersection-&-union.cpp
+++ Arrays/6-intersection-&-union.cpp
@@ -2,7 +2,8 @@
 class Solution{
     public:
     //Function to return the count of number of elements in union of two arrays.
-    int doUnion(int a[], int n, int b[], int m)  {
+    int doUnion(int a[], int n, int b[], int m) 
+    {
         set<int >s;
         for(int i=0;i<n;i++)
         s.insert(a[i]);
@@ -10,7 +11,22 @@ class Solution{
         for(int i=0;i<m;i++)
         s.insert(b[i]);
         
-        return s.size();
-        
+        return s.size();        
+    }    // used sets as in the set every value is unique 
+    int doIntersection(int a[], int n, int b[], int m)  
+    {
+        unordered_set<int >s;
+        int ctr;
+        for(int i=0;i<n;i++)
+        s.insert(a[i]);
+        for(int i=0;i<m;i++)
+        {
+            if(s.find(b[i])!=s.end())
+            {
+                 ctr++;
+                 s.erase(b[i]);
+            }
+        }
+        return ctr;        
     }
-};
+};    // used onoredered set as all the operations takes O(1) and it's faster to search for a element
