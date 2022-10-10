234b3931b69ae1821917ff38ad5759f729a84b73
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-10-10 22:42:26 +0530

    Create 15_SearchIn2D_SortedMatrix.cpp

---
+++ Stiever's DSA sheet/Arrays/Easy/15_SearchIn2D_SortedMatrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)    // T.C. = O(log(m+n))  //USING BINARY SEARCH
+    {
+       
+       int n = matrix.size();
+        int m = matrix[0].size();
+        int l = 0, r = m * n - 1;
+        while (l != r){
+            int mid = (l + r - 1) >> 1;
+            if (matrix[mid / m][mid % m] < target)
+                l = mid + 1;
+            else 
+                r = mid;
+        }
+        return matrix[r / m][r % m] == target;
+    }
+        
+     bool searchMatrix(vector<vector<int>>& matrix, int target) // T.C. = O(m+n)  //USING BINARY SEARCH TREE
+     {
+        int rows = matrix.size(),
+			cols = matrix[0].size(),
+            row = 0, col = cols - 1;
+			
+        while (row < rows && col > -1) {
+            int cur = matrix[row][col];
+            if (cur == target) return true;
+            if (target > cur) row++;
+            else col--;
+        }
+        
+        return false;
+    }
+};
