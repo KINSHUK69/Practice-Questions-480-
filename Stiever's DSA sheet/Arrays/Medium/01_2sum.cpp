e5e0b9de3ba7128245714f027a16c1b3a0de51d7
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-10-11 22:54:26 +0530

    Update 01_2sum.cpp

--- Stiever's DSA sheet/Arrays/Medium/01_2sum.cpp
+++ Stiever's DSA sheet/Arrays/Medium/01_2sum.cpp
@@ -1,1 +1,43 @@
-code
+/*
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+ vector<int> twoSum(vector<int>& nums, int target)    // T.C. = O(n^2)
+    {
+        vector<int> temp;
+        for(auto i=0;i!=nums.size();i++)
+        {
+             for(auto j=0;j!=nums.size();j++)
+             {
+                 if(i!=j && nums[i]+nums[j]==target)
+                 {
+                    temp.push_back(j);
+                    temp.push_back(i);
+                     break;
+                      
+                 }
+                  break;
+            
+             }
+        
+       }
+        return temp;
+}
+
+vector<int> twoSum(vector<int>& nums, int target) // T.C. = O(n) S.C. = O(n)
+{
+        vector<int>arr;
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i])!=m.end())
+            {
+                arr.push_back(i);
+                arr.push_back(m[target-nums[i]]);
+                return arr;
+            }
+            m[nums[i]]=i;
+        }
+        return arr;
+    }
