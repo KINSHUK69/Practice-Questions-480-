1d65d548852340bc7f8206a768298f2ff6619298
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-10-15 22:35:21 +0530

    Update 06_Rearrange_Array_Elements_by_Sign.cpp

--- Stiever's DSA sheet/Arrays/Medium/06_Rearrange_Array_Elements_by_Sign.cpp
+++ Stiever's DSA sheet/Arrays/Medium/06_Rearrange_Array_Elements_by_Sign.cpp
@@ -2,3 +2,23 @@
 Input: nums = [3,1,-2,-5,2,-4]
 Output: [3,-2,1,-5,2,-4]
 */
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums)  // T.C. = O(n)  S.C. = O(1)
+    {
+       vector<int>ans(nums.size());
+        int even=0,odd=1;
+        for(auto i:nums)
+        {
+            if(i>0){
+                ans[even]=i;
+                even+=2;
+            }
+            if(i<0){
+                ans[odd]=i;
+                odd+=2;
+            }
+        }
+        return ans;
+    }
+};
