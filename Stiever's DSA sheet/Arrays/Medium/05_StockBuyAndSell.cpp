f76b5a4a24c9af7ff785cd8e0992d560a2dfd1b1
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-10-15 22:09:38 +0530

    Update 05_StockBuyAndSell.cpp

--- Stiever's DSA sheet/Arrays/Medium/05_StockBuyAndSell.cpp
+++ Stiever's DSA sheet/Arrays/Medium/05_StockBuyAndSell.cpp
@@ -4,3 +4,37 @@ Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 */
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) // T.C. = O(N^2)
+    {
+        int max_price=INT_MIN,min_price=INT_MIN,profit,m=0;
+        int i=0,n=prices.size(),j=0;
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+               if(prices[j]>prices[i]) 
+                 m=max(m,prices[j]-prices[i]);
+            }
+        }
+        return (m>0)?m:0;
+    }
+  
+    int maxProfit(vector<int>& prices)   // T.C. = O(N)
+    {
+        int min_price=INT_MAX,m=INT_MIN;
+        int i=0,n=prices.size(),j=0;
+        for(i=0;i<n;i++)
+        {
+            if(prices[i]<min_price)
+              min_price=prices[i];
+            m=max(m,prices[i]-min_price);
+            
+        }
+        return (m>0)?m:0;
+    }
+};
