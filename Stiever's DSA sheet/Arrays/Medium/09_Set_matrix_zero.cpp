c02a1a98734bfb879d4740653cec24bb2aa81b17
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-10-18 22:18:53 +0530

    Update 09_Set_matrix_zero.cpp

--- Stiever's DSA sheet/Arrays/Medium/09_Set_matrix_zero.cpp
+++ Stiever's DSA sheet/Arrays/Medium/09_Set_matrix_zero.cpp
@@ -39,19 +39,19 @@ void setZeroes(int[][] matrix)    // T.C. = O(M*N*K)
 //  T.C. = O(MN)
 void setZeroes(int[][] matrix)   
 {
- int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+ int rows = matrix.size(), cols = matrix[0].size();
+ bool col0=true;
 
     for (int i = 0; i < rows; i++) {
         if (matrix[i][0] == 0) col0 = 0;
         for (int j = 1; j < cols; j++)
-            if (matrix[i][j] == 0)
-                matrix[i][0] = matrix[0][j] = 0;
+            if (matrix[i][j] == 0) matrix[i][0] = matrix[0][j] = 0;
     }
 
     for (int i = rows - 1; i >= 0; i--) {
         for (int j = cols - 1; j >= 1; j--)
-            if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                matrix[i][j] = 0;
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+	    
         if (col0 == 0) matrix[i][0] = 0;
     }
 }
