d920cebdc82dcbbd93aa0c8633a1237037f0559d
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-10-28 23:13:47 +0530

    Update 08_Find_min_in_rotated_array.cpp

--- Stiever's DSA sheet/Binary Search/learning BS/08_Find_min_in_rotated_array.cpp
+++ Stiever's DSA sheet/Binary Search/learning BS/08_Find_min_in_rotated_array.cpp
@@ -10,5 +10,19 @@ public:
         sort(nums.begin(),nums.end());
         return nums[0];
     }
+    
+    int findMin(vector<int>& nums) // T.C. = O(logN)
+    {
+        // loop            1. low < high
+        //                 2. mid != high and thus A[mid] != A[high] (no duplicate exists)
+        //                 3. minimum is between [low, high]
+        int start=0,end=nums.size()-1,mid;
+        while(start<end){
+            mid=(start+end)>>1;
+            if(nums[mid]<nums[end]) end=mid;            // the mininum is in the left part
+            if(nums[mid]>nums[end]) start=mid+1;        // the mininum is in the right part           
+        }
+        return nums[start];
+    }
   
 };
