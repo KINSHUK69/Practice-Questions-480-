baad15bde446108dad02d58c3e1f6a2a8daece76
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-10-31 22:55:00 +0530

    Update 09_Single_Element_in_a_Sorted_Array.cpp

--- Stiever's DSA sheet/Binary Search/learning BS/09_Single_Element_in_a_Sorted_Array.cpp
+++ Stiever's DSA sheet/Binary Search/learning BS/09_Single_Element_in_a_Sorted_Array.cpp
@@ -26,4 +26,25 @@ public:
         
         return sum;
     }
+    
+    /*
+    So, the array has all the elements repeating twice except for one element which appears only once and the array is sorted.
+    This means that in every number that's repeated, the first number is at an even index (index%2==0) and the 2nd number is at an odd index.
+    The idea is to peform a binary search over the entire array and find out if this pattern follows. 
+       ->If somewhere this pattern is broken, then we're going to know in which half of the array there is an element that appears only once.
+    We reduce the search space to that half an search again, until we're left with a single element, which is the final answer
+    */
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        if(nums.size()==1) return nums[0];
+        int start=0, end=nums.size()-1, mid, num;
+        
+        while(start < end){
+            mid = (start+end)>>1;
+            num = (mid%2 == 0) ? mid+1 : mid-1;
+            if(nums[mid]==nums[num]) start = mid+1;
+            else end = mid;
+        }
+        return nums[start];
+    }
 };
