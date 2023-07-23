0b37ddd58e783ccc0e83ab8c7703266f6902ffc6
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-07-23 22:35:44 +0530

    Update 12_Subarray_sum_equals_k.cpp

--- Stiever's DSA sheet/Arrays/Medium/12_Subarray_sum_equals_k.cpp
+++ Stiever's DSA sheet/Arrays/Medium/12_Subarray_sum_equals_k.cpp
@@ -17,8 +17,41 @@ public:
                     if(s==k)ans++;
             }
         }
+        return ans;        
+    }
+};
+
+// T.C. = O(n^2)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int ans=0,n=nums.size();
+        for(int i=0;i<n;i++){
+                int s=0;
+                for(int j=i+1;j<=n;j++){
+                    s+=nums[j];
+                }
+                    if(s==k)ans++;
+            }
+        }
+        return ans;        
+    }
+};
+
+ 
+// T.C. = O(n)  S.C.= O(n)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>m;
+        int sum=0,ans=0;
+        m[0]=1;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(m.find(sum-k)!=m.end())ans+=m[sum-k];
+            m[sum]++;
+        }
         return ans;
 
-        
     }
 };
