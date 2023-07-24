962dd499786240bd869f4f51b66240651194f43e
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-07-24 23:14:08 +0530

    Update 01_PascalsTriangle.cpp

--- Stiever's DSA sheet/Arrays/Hard/01_PascalsTriangle.cpp
+++ Stiever's DSA sheet/Arrays/Hard/01_PascalsTriangle.cpp
@@ -1,1 +1,15 @@
-code
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        // if(numRows== 1)return {1};
+        vector<vector<int>>ans;
+        for(int i=0;i<numRows;i++){
+            vector<int>temp(i+1,1);
+            for(int j=1;j<i;j++){
+                temp[j]=ans[i-1][j]+ans[i-1][j-1];
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
