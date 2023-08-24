a18738e1809e2ddbbed60eef9954b5cd22119cde
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-08-25 01:04:33 +0530

    Update 01_Reverse_LL_in_group_of_given_size_K.cpp

--- Stiever's DSA sheet/Linked List/Hard/Linked List/01_Reverse_LL_in_group_of_given_size_K.cpp
+++ Stiever's DSA sheet/Linked List/Hard/Linked List/01_Reverse_LL_in_group_of_given_size_K.cpp
@@ -1,1 +1,41 @@
-code
+// T.C. = O((n/k)*k) = O(n)
+class Solution {
+public:
+
+    int size(ListNode* head) {
+        ListNode* dummy=head;
+        int length=0;
+        while(dummy!=NULL){
+            length++;
+            dummy=dummy->next;
+        }
+        return length;
+        
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL)return head;
+
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* prev=dummy;
+        ListNode* curr;
+        ListNode* nex;
+
+        int len=size(head);
+
+        while(len>=k){
+            curr=prev->next;
+            nex=curr->next;
+            for(int i=1;i<k;i++){
+                curr->next=nex->next;
+                nex->next=prev->next;
+                prev->next=nex;
+                nex=curr->next;
+            }
+            prev=curr;
+            len-=k;
+        }
+        return dummy->next;
+
+    }
+};
