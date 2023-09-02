9f12409a512fec9d586d7df51f1b95ca052475fd
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-09-03 00:44:55 +0530

    Update 04_Min_Stack.cpp

--- Stiever's DSA sheet/Stack and Queues /Learning/04_Min_Stack.cpp
+++ Stiever's DSA sheet/Stack and Queues /Learning/04_Min_Stack.cpp
@@ -1,3 +1,4 @@
+// T.C.= O(1) S.C.= O(2N)
 class MinStack {
     stack<pair<int,int> >st;
 public:
@@ -25,7 +26,47 @@ public:
         return st.top().second;
     }
 };
+// T.C.= O(1) S.C.= O(1)
+class MinStack {
+    stack<long long>st;
+    long long minimum;
+public:
 
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        long long v=val;
+        
+        if(st.empty()){
+            minimum=val;
+            st.push(val);
+        }
+        else if(minimum>val){
+            st.push(2*v-minimum);
+            minimum=val;
+        }
+        else 
+            st.push(v);
+
+    }
+    
+    void pop() {
+        if(st.top()<minimum)
+            minimum=2*minimum-st.top();
+        st.pop();
+    }
+    
+    int top() {
+        if(minimum>st.top())return minimum;
+        return st.top();
+    }
+    
+    int getMin() {
+        return minimum;
+    }
+};
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
