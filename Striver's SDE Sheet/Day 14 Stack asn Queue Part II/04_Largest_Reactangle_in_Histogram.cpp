7ec230818a673e3667f6ac9c75c097b95b1d39d0
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-09-25 14:49:32 +0530

    Update 04_Largest_Reactangle_in_Histogram.cpp

--- Striver's SDE Sheet/Day 14 Stack asn Queue Part II/04_Largest_Reactangle_in_Histogram.cpp
+++ Striver's SDE Sheet/Day 14 Stack asn Queue Part II/04_Largest_Reactangle_in_Histogram.cpp
@@ -19,3 +19,30 @@ public:
         return maxArea; 
     }
 };
+// Optimal Approach 
+// T.C. = O(N)
+// S.C. = O(N)
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int maxArea = 0;
+        stack<int>stack;
+        for (int i = 0; i < heights.size(); ++i) {
+            while (!stack.empty() && heights[i] < heights[stack.top()]) {
+                int height = heights[stack.top()];
+                stack.pop();
+                int width = stack.empty() ? i : i - stack.top() - 1;
+                maxArea = max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        while (!stack.empty()) {
+            int height = heights[stack.top()];
+            stack.pop();
+            int width = stack.empty() ? heights.size() : heights.size() - stack.top() - 1;
+            maxArea = max(maxArea, height * width);
+        }
+        return maxArea;
+    }
+};
