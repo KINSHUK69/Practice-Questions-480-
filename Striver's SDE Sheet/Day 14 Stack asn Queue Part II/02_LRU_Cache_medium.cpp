29281e46c7c307032c663274a457b150bb50182f
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-09-28 00:59:51 +0530

    Update 02_LRU_Cache_medium.cpp

--- Striver's SDE Sheet/Day 14 Stack asn Queue Part II/02_LRU_Cache_medium.cpp
+++ Striver's SDE Sheet/Day 14 Stack asn Queue Part II/02_LRU_Cache_medium.cpp
@@ -1,4 +1,6 @@
-// Using DEQUEUE
+// Brute Force : Using DEQUEUE
+// TLE 
+// test case passed 20/22
 class LRUCache {
 public:
     int max=0; 
@@ -37,9 +39,82 @@ public:
     }
 };
 
+// Optimal : Using DoublyLinkedList
+
+class LRUCache {
+public:
+    class node{
+        public:
+            int key;
+            int val;
+            node* next;
+            node* prev;
+
+            node(int key,int val){
+                this->key=key;
+                this->val=val;
+            }
+    };
+    int max=0; 
+    node* tail = new node(-1,-1);
+    node* head = new node(-1,-1);
+
+    unordered_map<int,node*>mp;
+
+    LRUCache(int capacity) {
+        max=capacity;
+        head->next=tail;
+        tail->prev=head;
+    }
+    void addNode(node* newNode){
+        node* temp=head->next;
+        newNode->next=temp;
+        newNode->prev=head;
+        head->next=newNode;
+        temp->prev=newNode;
+    }
+    void deleteNode(node * del){
+        node* delprev=del->prev;
+        node* delnext=del->next;
+        delprev->next=delnext;
+        delnext->prev=delprev;
+    }
+    
+    int get(int key) {
+        if(mp.find(key)!= mp.end()){
+            node* temp = mp[key];
+            int ans = temp->val;
+            mp.erase(key);
+            deleteNode(temp);
+            addNode(temp);
+            mp[key] = head->next;
+            return ans;
+        }        
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()){
+            node* temp = mp[key];
+            mp.erase(key);
+            deleteNode(temp);
+        }
+        
+        if(mp.size() == max){
+            mp.erase(tail->prev->key);
+            deleteNode(tail->prev);
+        }
+        addNode(new node(key,value));
+        mp[key] = head->next;
+        
+    }
+};
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+
