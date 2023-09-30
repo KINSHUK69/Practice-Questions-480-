705d42293d3cddb20ed078c0fe49644226a99f18
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-10-01 01:21:03 +0530

    Update 09_Single_Element_in_a_Sorted_Array.cpp

--- Stiever's DSA sheet/Binary Search/learning BS/09_Single_Element_in_a_Sorted_Array.cpp
+++ Stiever's DSA sheet/Binary Search/learning BS/09_Single_Element_in_a_Sorted_Array.cpp
@@ -48,3 +48,24 @@ public:
         return nums[start];
     }
 };
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size() ;
+        int left=0,right=n-2;
+
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        while(left<=right){
+            int mid=(left+right)>>1;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) 
+                return nums[mid];
+            else if( (mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1]) )     
+                left=mid+1; 
+            else right=mid-1;            
+        }
+        return -1;
+    }
+};
