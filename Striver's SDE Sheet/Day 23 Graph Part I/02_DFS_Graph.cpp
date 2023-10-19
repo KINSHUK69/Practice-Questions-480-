c727d4b2d32af1c34c8aa67dbd7592fc9d9f63b8
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-10-20 00:35:22 +0530

    Create 02_DFS_Graph.cpp

---
+++ Striver's SDE Sheet/Day 23 Graph Part I/02_DFS_Graph.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    
+    void dfs(int start,vector<int> adj[],vector<int>&vis,vector<int>&ans){
+        vis[start]=1;
+        ans.push_back(start);
+        for(auto it:adj[start]){
+            if(!vis[it]){
+                dfs(it,adj,vis,ans);
+            }
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>ans;
+        vector<int>vis(V,0);
+        
+        dfs(0,adj,vis,ans);
+        return ans;
+    }
+};
