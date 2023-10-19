cfb9c596dbec9b7b69fa6cee7281e91846b88ab9
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-10-19 18:55:17 +0530

    Update 06_Repeated_String_Match__rabin-karp-algorithm-for-pattern-searching.cpp

--- Striver's SDE Sheet/Day 15 String Part I/06_Repeated_String_Match__rabin-karp-algorithm-for-pattern-searching.cpp
+++ Striver's SDE Sheet/Day 15 String Part I/06_Repeated_String_Match__rabin-karp-algorithm-for-pattern-searching.cpp
@@ -25,3 +25,57 @@ public:
         return -1;
     }
 };
+
+// Using Rabin Karp Algo for String matching creating hash funtion for a and b and only checking the substring when the hash function matches 
+#define ll long long
+class Solution {
+public:
+
+    bool RabinKarp(string source,string target){
+        ll p=31, m=1e9 + 9;
+        int S=source.size(), T=target.size();
+        
+        vector<ll> power(max(S,T), 1);
+
+        for(int i=1;i<power.size(); i++){
+            power[i]=(power[i-1] * p) % m;
+        }
+
+        vector<ll> H(T + 1, 0);
+        for(int i=0;i<T;i++){
+            H[i+1]=(H[i] + (target[i]-'a'+1) * power[i]) % m;
+        }
+        
+        ll HS=0;
+
+        for(int i=0;i<S;i++){    
+            HS=(HS + (source[i]-'a'+ 1)*power[i]) % m;
+        }
+        
+        ll currHS=0;
+        for(int i=0;i+S<=T;i++)
+        {
+            currHS=(H[i+S] - H[i] + m) % m;
+            if(currHS == HS*power[i]%m)
+                return true;
+        }
+        return false;
+    }
+    int repeatedStringMatch(string a, string b) {       
+        if(a==b)
+        return 1;
+
+        int count=1;
+        string s=a;
+
+        while(s.size()<b.size()){
+            s+=a;
+            count++;
+        }
+        if(s==b) return count;
+        if(RabinKarp(b,s)) return count;
+        if(RabinKarp(b,s+a)) return count+1;
+
+        return -1;
+    }
+};
