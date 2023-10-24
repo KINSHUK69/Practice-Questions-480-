23dde677fabcb53abb1ef5de5e5cec5f4d8f8b89
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-10-25 01:35:05 +0530

    Update 03_Diameter_of_BT.cpp

--- Striver's SDE Sheet/Day 18 Binary Tree Part II/03_Diameter_of_BT.cpp
+++ Striver's SDE Sheet/Day 18 Binary Tree Part II/03_Diameter_of_BT.cpp
@@ -1,1 +1,29 @@
-code
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root,int &d){
+        if(!root)return 0;
+
+        int left=dfs(root->left,d);
+        int right=dfs(root->right,d);
+
+        d=max(d,left+right);
+
+        return max(left,right)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int d=0;
+        dfs(root,d);
+        return d;
+    }
+};
