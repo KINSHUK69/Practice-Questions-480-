b0dea401ca0a48e462bf8099a6e7de00e0fd1b4d
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-11-04 02:26:02 +0530

    Update 03_Kth_Smallest_in_BST.cpp

--- Striver's SDE Sheet/Day 21 Binary Search Tree Part II/03_Kth_Smallest_in_BST.cpp
+++ Striver's SDE Sheet/Day 21 Binary Search Tree Part II/03_Kth_Smallest_in_BST.cpp
@@ -9,19 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// USING INORDER TRAVERSAL : inorder traversal of BST gives a perfectly sorted array
+// T.C. = O(n)
+// S.C. = O(n)
 class Solution {
 public:
     void dfs(TreeNode* root,vector<int>&v){
         if(!root)return;
-
         dfs(root->left,v);
         v.push_back(root->val);
         dfs(root->right,v);
     }
     int kthSmallest(TreeNode* root, int k) {
         vector<int>in;
-        dfs(root,in);
-        for(auto it:in)cout<<it<<" ";
+        dfs(root,in);        
         return in[k-1];
+    }
+};
+// USING INORDER TRAVERSAL : starting from the left most 
+// T.C. = O(min(k,n))
+// S.C. = O(min(k,n))
+class Solution {
+public:
+    
+    void solve(TreeNode* root,int& ctr,int& ans,int& k){
+       if(!root)return;
+
+       solve(root->left,ctr,ans,k);
+       ctr++;
+
+       if(ctr==k){
+           ans=root->val;
+           return;
+       }
+
+       solve(root->right,ctr,ans,k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+       int ctr=0,ans;
+       solve(root,ctr,ans,k);
+       return ans;
     }
 };
