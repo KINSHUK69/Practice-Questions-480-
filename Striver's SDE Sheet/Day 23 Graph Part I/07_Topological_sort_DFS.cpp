53f5bfaa9c6a029b8717ba6ed27d861e79a00409
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-11-17 00:37:39 +0530

    Create 07_Topological_sort_DFS.cpp

---
+++ Striver's SDE Sheet/Day 23 Graph Part I/07_Topological_sort_DFS.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+	public:
+	void dfs(int node,vector<int>&vis,vector<int>adj[],stack<int>&s){
+	    vis[node]=1;
+	    
+	    for(auto it:adj[node]){
+	        if(!vis[it]){
+	            dfs(it,vis,adj,s);
+	        }
+	    }
+	    s.push(node);
+	}
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>vis(V,0);
+	    stack<int>s;
+	    
+	    for(int i=0;i<V;i++){
+	        if(!vis[i]){
+	            dfs(i,vis,adj,s);
+	        }
+	    }
+
+	    vector<int>ans;
+	    
+	    while(!s.empty()){
+	        ans.push_back(s.top());
+	        s.pop();
+	    }
+	    return ans;
+	}
+};
