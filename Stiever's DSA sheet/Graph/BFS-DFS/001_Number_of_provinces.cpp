5bddf6eea9e97f2044580b6bb110060bc8761334
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-11-18 22:53:42 +0530

    Create 001_Number_of_provinces.cpp

---
+++ Stiever's DSA sheet/Graph/BFS-DFS/001_Number_of_provinces.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void dfs(int node,vector<int>adj[],vector<int>&vis){
+        vis[node]=1;
+        
+        for(auto it:adj[node]){
+            if(!vis[it]) dfs(it,adj,vis);
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& m) {
+        int n=m.size();
+        vector<int>vis(n,0);
+        int ans=0;
+
+        vector<int>adj[n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(m[i][j]==1 && i!=j){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,adj,vis);
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
