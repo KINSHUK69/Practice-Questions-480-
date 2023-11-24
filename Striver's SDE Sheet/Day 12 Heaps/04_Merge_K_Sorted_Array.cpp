ebd8119d30de75bac46a881b582c426458c3a402
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2023-11-24 15:07:49 +0530

    Create 04_Merge_K_Sorted_Array.cpp

---
+++ Striver's SDE Sheet/Day 12 Heaps/04_Merge_K_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+vector<int> mergeKSortedArrays(vector<vector<int>>&arr, int k)
+{
+    priority_queue<int,vector<int>,greater<int>>pq; 
+    for(int i=0;i<arr.size();i++){
+        for(int j=0;j<arr[i].size();j++)pq.push(arr[i][j]);
+    }
+
+    vector<int>ans;
+
+    while(!pq.empty()){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    return ans;
+}
