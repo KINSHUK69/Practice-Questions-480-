22896ba33958b751219368f69f25bce0c0fb1469
Kinshuk Dewari <71977053+kinshuk-dewari@users.noreply.github.com>
2023-12-02 23:25:46 +0530

    Create 02_Longest_Increasing_Subsequence.cpp

---
+++ Striver's SDE Sheet/Day 25 Dynamic Programming Part I/02_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // T.L.E 
+    // T.C. = O(2^n)
+    // S.C. = O(N)
+
+    int solve(int i,int pre,vector<int>&nums,vector<int>&ds){
+        if(i==nums.size())return 0;
+
+        int len=solve(i+1,pre,nums,ds);
+        if(pre==-1 || nums[i]>nums[pre]) len=max(len,1+solve(i+1,i,nums,ds));
+        return len;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>ds;
+        return solve(0,-1,nums,ds);
+    }
+
+    // Memoization
+    // T.C. = O(N*N)
+    // S.C. = O(N^2) + O(N)
+    int solve(int i,int pre,vector<int>&nums,vector<vector<int>>&dp){
+        if(i==nums.size())return 0;
+
+        if(dp[i][pre+1]!=-1)return dp[i][pre+1];
+
+        int len=solve(i+1,pre,nums,dp);
+
+        if(pre==-1 || nums[i]>nums[pre]) len=max(len,1+solve(i+1,i,nums,dp));
+        
+        dp[i][pre+1]=len;
+        return dp[i][pre+1];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return solve(0,-1,nums,dp);
+    }
+};
