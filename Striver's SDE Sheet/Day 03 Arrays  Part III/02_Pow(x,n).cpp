374a2932c3a6a977ec18584719c69e8995c78406
Kinshuk Dewari <71977053+kinshuk-dewari@users.noreply.github.com>
2023-12-08 21:14:03 +0530

    Create 02_Pow(x,n).cpp

---
+++ Striver's SDE Sheet/Day 03 Arrays  Part III/02_Pow(x,n).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        if(n==0) return double(1.0);
+        if(n==1) return x;
+        long long nn = n;
+        if(n < 0) nn = -1*nn;
+        while(nn){
+            if(nn%2!=0){
+                ans*=x;
+                nn-=1;
+            }else{
+                x*=x;
+                nn/=2;
+            }
+        }
+        if(n < 0) ans = double(1.0)/double(ans);
+        return ans;
+    }
+};
